0268fe558141c9e00d21d6e548918f8d7eb4449d
bentom08 <bentom06@hotmail.co.uk>
2018-11-11 16:31:02 +0000

    login completed, other than login session storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-import Login from './login.js'
+import Login from './Login.js'
 
 const port = 8081;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      user: 'ben'
+    })
+  }
   render() {
     return (
-      <Login />
+      <Login user = {this.state.user} />
     );
   }
 }
--- src/Login.js
+++ src/Login.js
@@ -7,11 +7,28 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = ({
-      usernameMsg: ''
+      usernameMsg: '',
+      username: this.props.user,
+      signUp: false,
+      loggedIn: false
     })
   }
 
+  passwordsMatch = () => {
+    if (document.getElementById('password') === document.getElementById('reenter')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   signUp = () => {
+    if (!this.passwordsMatch()) {
+      this.setState({
+        usernameMsg: 'Passwords do not match'
+      })
+      return;
+    }
     axios.get("http://localhost:"+ port +"/battleships-1.0/api/battleships/checkUsername/" + document.getElementById('username').value).then((response) => {
 
       if (response.data.response === "false") {
@@ -22,8 +39,10 @@ class Login extends Component {
           }
         ).then(
           this.setState({
-            usernameMsg: ''
+            loggedIn: true,
+            signUp: false
           })
+          //TODO store login in Parent via either cookie or back end
         );
       } else {
         this.setState({
@@ -48,7 +67,10 @@ class Login extends Component {
                 usernameMsg: 'Incorrect password'
               })
             } else {
-              //TODO: Sign in
+              this.setState({
+                loggedIn: true
+              })
+              //TODO store login in Parent via either cookie or back end
             }
           }
         );
@@ -60,15 +82,51 @@ class Login extends Component {
     })
   }
 
+  goToSignUp = () => {
+    this.setState({
+      signUp: true
+    })
+  }
+
+  signOut = () => {
+    this.setState({
+      loggedIn: false,
+    })
+    //TODO remove login in Parent via either cookie or back end
+  }
+
   render() {
-    return (
-      <div>
-        <input id='username' type = 'text' placeholder='username...' /><br/><br/>
-        <input id ='password' type='password' placeholder='password...' /><br/><br/>
-        <input id = 'login' value = 'Log In' type = 'button' onClick =  {this.login} />
-        <input id = 'signUp' value = 'Sign Up' type = 'button' onClick =  {this.signUp} /><br/>
-        {this.state.usernameMsg}
+    const login = <div>
+      Username: <input id='username' type = 'text' placeholder='username...' /><br/><br/>
+      Password: <input id ='password' type='password' placeholder='password...' /><br/><br/>
+      <input id = 'login' value = 'Log In' type = 'button' onClick =  {this.login} />
+      <input id = 'signUp' value = 'Sign Up' type = 'button' onClick =  {this.goToSignUp} /><br/>
+      <p style={{color:'red'}}>{this.state.usernameMsg}</p>
+    </div>
+
+    const signUp = <div>
+      Username: <input id='username' type = 'text' placeholder='username...' /><br/><br/>
+      Password: <input id ='password' type='password' placeholder='password...' /><br/><br/>
+      Re-enter Password: <input id ='reenter' type='password' placeholder='password...' /><br/><br/>
+      <input id = 'signUp' value = 'Sign Up' type = 'button' onClick =  {this.signUp} /><br/>
+      <p style={{color:'red'}}>{this.state.usernameMsg}</p>
+    </div>
+
+    const loggedIn = <div>
+      You are currently logged in as {this.state.user}<br/>
+      <input id = 'signOut' value = 'Sign Out' type = 'button' onClick =  {this.signOut} />
       </div>
+
+    if (this.state.signUp === "true") {
+      var display = signUp;
+    } else if (this.state.loggedIn === "true") {
+      var display = loggedIn;
+    } else {
+      var display = login;
+    }
+
+    return (
+      display
     );
   }
 }
